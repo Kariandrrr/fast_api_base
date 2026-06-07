2c2dd3bc422816ea674189dac101bcff8ddd648a
Kariandrrr <educationkarina08@gmail.com>
2026-06-07 15:36:08 +0300

    models (imports): teacher and teacher_availability

--- backend/app/core/models/__init__.py
+++ backend/app/core/models/__init__.py
@@ -7,9 +7,11 @@ __all__ = (
     "Room",
     "TimeSlot",
     "Semester",
-
+    "Teacher",
+    "TeacherAvailability",
 )
 from .helpers import db_helper, redis_helper, Base
 from .user import User
 from .building import Building, Room
 from .time import TimeSlot, Semester
+from .teacher import Teacher, TeacherAvailability
--- backend/app/enums/__init__.py
+++ backend/app/enums/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "RoomType",
     "SlotNumber",
+    "DayOfWeek",
 )
 
 from .room_type import RoomType
 from .time_slots import SlotNumber
+from .days_of_week import DayOfWeek
