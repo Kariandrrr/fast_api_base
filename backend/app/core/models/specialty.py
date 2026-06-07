92427bc99c9d251d1d83b47783db1f6fa89ac310
Kariandrrr <educationkarina08@gmail.com>
2026-06-07 15:48:40 +0300

    models: specialty

---
+++ backend/app/core/models/specialty.py
@@ -0,0 +1,16 @@
+from sqlalchemy import (
+    String,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .helpers import Base
+from .mixins import UUIDPKMixin
+
+
+class Specialty(Base, UUIDPKMixin):
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    code: Mapped[str] = mapped_column(String(15), nullable=False)
+
+    __table_args__ = (UniqueConstraint("name", "code", name="uq_specialty_name_code"),)
+    # TODO: rel
