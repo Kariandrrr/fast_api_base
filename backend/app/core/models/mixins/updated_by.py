560e6087391b552564847326041c187c588972bb
Kariandrrr <educationkarina08@gmail.com>
2026-06-18 14:52:31 +0300

    add mixin updated_by, created_by

---
+++ backend/app/core/models/mixins/updated_by.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class UpdatedByMixin:
+    updated_by: Mapped[UUID | None] = mapped_column(
+        ForeignKey("users.id", ondelete="SET NULL"),
+        nullable=False,
+    )
