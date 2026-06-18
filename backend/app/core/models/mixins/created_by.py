9664f0d941e1b50045f882cbe3e080fe1649e11a
Kariandrrr <educationkarina08@gmail.com>
2026-06-18 14:59:07 +0300

    null = True

--- backend/app/core/models/mixins/created_by.py
+++ backend/app/core/models/mixins/created_by.py
@@ -7,5 +7,5 @@ from sqlalchemy.orm import Mapped, mapped_column
 class CreatedByMixin:
     created_by: Mapped[UUID | None] = mapped_column(
         ForeignKey("users.id", ondelete="SET NULL"),
-        nullable=False,
+        nullable=True,
     )
--- backend/app/core/models/mixins/updated_by.py
+++ backend/app/core/models/mixins/updated_by.py
@@ -7,5 +7,5 @@ from sqlalchemy.orm import Mapped, mapped_column
 class UpdatedByMixin:
     updated_by: Mapped[UUID | None] = mapped_column(
         ForeignKey("users.id", ondelete="SET NULL"),
-        nullable=False,
+        nullable=True,
     )
