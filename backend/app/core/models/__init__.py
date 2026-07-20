748b4001bc5513ba74a0a5c8d4b9b89c4d922949
Suslova Karina <educationkarina08@gmail.com>
2026-07-20 16:24:20 +0300

    Update __init__.py

--- backend/app/core/models/__init__.py
+++ backend/app/core/models/__init__.py
@@ -3,29 +3,6 @@ __all__ = (
     "redis_helper",
     "Base",
     "User",
-    "Building",
-    "Room",
-    "TimeSlot",
-    "Semester",
-    "Teacher",
-    "TeacherAvailability",
-    "TeacherSubstitution",
-    "Speciality",
-    "Group",
-    "Practice",
-    "Subject",
-    "SchedulePlan",
-    "ScheduleItem",
-    "TeacherSubject",
 )
 from .helpers import db_helper, redis_helper, Base
 from .user import User
-from .building import Building, Room
-from .time import TimeSlot, Semester
-from .teacher import Teacher, TeacherAvailability, TeacherSubstitution
-from .speciality import Speciality
-from .group import Group
-from .practice import Practice
-from .subject import Subject
-from .schedule import SchedulePlan, ScheduleItem
-from .teacher_subjects import TeacherSubject
